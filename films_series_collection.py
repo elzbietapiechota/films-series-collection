38177aa75cec91e830f47c4cfcadaf0a98c09d3c
elzbietapiechota <ela.polakiewicz@gmail.com>
2026-02-14 19:22:12 +0100

    Add collection

---
+++ films_series_collection.py
@@ -0,0 +1,170 @@
+import random
+from faker import Faker
+from datetime import datetime
+
+fake = Faker()
+
+
+class BaseTvContent:
+    
+   def __init__(self, title, year, genre, views):
+        self.title = title
+        self.year = year
+        self.genre = genre
+        self.views = views
+   
+   def play(self, step=1):
+       self.views += step
+       return self.views 
+  
+class Movies(BaseTvContent):
+   
+   def __init__(self, *args, **kwargs):
+       super().__init__(*args, **kwargs)
+    
+   def __str__(self):
+       return f"{self.title} ({self.year})"
+   
+class Series(BaseTvContent):
+   
+   def __init__(self, episode, season, *args, **kwargs):
+       super().__init__(*args, **kwargs)
+       self.episode = episode
+       self.season = season     
+    
+   def __str__(self):
+       return f"{self.title} S{self.season}E{self.episode}"  
+
+
+content = []
+
+def create_content():
+
+    for movie_title in movie_titles:
+
+        movies = Movies(
+            title=movie_title, 
+            year=fake.year(), 
+            genre=random.choice(genres), 
+            views=fake.random_int(min=0, max=100))
+        content.append(movies)
+
+    for series_title in series_titles:  
+
+        series = Series(
+            title=series_title, 
+            year=fake.year(), 
+            genre=random.choice(genres), 
+            episode=f"{random.randint(1, 20):02}",
+            season=f"{random.randint(1, 10):02}",
+            views=fake.random_int(min=0, max=100))
+        content.append(series)
+
+
+def get_movies():
+
+    by_movies = [item for item in content if isinstance(item, Movies)]
+    return sorted(by_movies, key=lambda item: item.title)
+
+def get_series():
+    
+    by_series = [item for item in content if isinstance(item, Series)]
+    return sorted(by_series, key=lambda item: item.title)
+
+def generate_views():
+
+    item_of_content = random.choice(content)
+    number_of_views = random.randint(1, 100)
+    
+    for i in range(number_of_views):
+        item_of_content.play()
+    
+def run_generate_views():
+    for i in range(10):
+        generated_views = generate_views()
+    return generated_views
+
+def top_titles(content_type, amount):
+    
+    if content_type == Movies:
+        filtered_top_titles = [item for item in content if isinstance(item, Movies)]
+    
+    elif content_type == Series:
+        filtered_top_titles = [item for item in content if isinstance(item, Series)]
+
+    else:
+        filtered_top_titles = content
+
+    by_views = sorted(filtered_top_titles, key=lambda item: item.views, reverse=True)
+    filtered_top_titles = by_views[:amount]
+
+    for item in filtered_top_titles:
+        print(f"{item}")
+
+def search(title):
+    search_result = [item for item in content if title.lower() in item.title.lower()]
+    
+    if search_result:
+        for item in search_result:
+            print(f"Tytuł {item} znajduje się w bibliotece")
+    else:
+        print(f"Niestety tytułu {title} nie ma w bibliotece")
+
+
+movie_titles = [
+    "Dzień świra", 
+    "Chłopaki nie płaczą", 
+    "Pianista", 
+    "Bogowie", 
+    "Zimna wojna", 
+    "Boże Ciało", 
+    "Katyń", 
+    "Killer", 
+    "Psy", 
+    "Wołyń"
+    ]
+
+series_titles = [
+    "Wataha",
+    "1670",
+    "Ranczo",
+    "Ślepnąc od świateł",
+    "Wielka woda",
+    "Kruk",
+    "Belfer",
+    "Król",
+    "Odwróceni",
+    "Rojst"
+    ]
+
+genres = [
+    "Dramat",
+    "Komedia",
+    "Kryminał",
+    "Sci-Fi",
+    "Thriller",
+    "Horror",
+    "Dokumentalny",
+    "Akcja",
+    "Fantasy",
+    "Historyczny"
+    ]
+
+
+if __name__ == '__main__':
+    create_content()
+
+    print("Biblioteka filmów:")   
+    for item in get_movies():
+        print(item)
+    print()
+
+    run_generate_views()
+
+    print(f"Najpopularniejsze filmy i seriale dnia {datetime.now().strftime('%d.%m.%Y')}:")
+    top_titles(None, 3)
+    print()
+
+    print("Wyszukiwanie tytułów w bibliotece:")
+    search("'Oczy'")
+    
